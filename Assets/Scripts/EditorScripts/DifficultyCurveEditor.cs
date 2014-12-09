f4a319152825d34cebad871dd46d82ff6b1eae52
Jon Lloyd <jonlloyd93@gmail.com>
2014-12-09 09:54:38 +0000

    Buttons etc for Editor

--- Assets/Scripts/EditorScripts/DifficultyCurveEditor.cs
+++ Assets/Scripts/EditorScripts/DifficultyCurveEditor.cs
@@ -6,5 +6,24 @@ using System.Collections;
 
 public class DifficultyCurveEditor : Editor{
 	public override void OnInspectorGUI(){
+		if (GUILayout.Button ("Open Difficulty File")) {
+			
+		}
+		if (GUILayout.Button ("Save Difficulty File")) {
+			
+		}
+		GUILayout.Label ("Select Blocks to Appear", EditorStyles.boldLabel);
+		if (GUILayout.Toggle(false,"1")) {
+
+		}
+		if (GUILayout.Toggle(false,"2")) {
+			
+		}
+		if (GUILayout.Toggle(false,"3")) {
+			
+		}
+		if (GUILayout.Toggle(false,"4")) {
+			
+		}
 	}
 }
